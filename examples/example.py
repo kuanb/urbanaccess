5357f3d698502852318184a2c843cbaf398af7cd
Kuan Butts <kuanbutts@gmail.com>
2017-04-20 15:43:38 -0700

    all the way through headways now working

--- examples/example.py
+++ examples/example.py
@@ -7,9 +7,9 @@ def continue_check(clarify=False):
     if clarify:
         message = 'Not a valid option. Try again.\n' + message
     response = raw_input(message)
-    if response in ['yY']:
+    if response in ['y', 'Y']:
         pass
-    elif response in ['nN']:
+    elif response in ['n', 'N']:
         print('Exiting...')
         sys.exit()
     else:
@@ -22,7 +22,6 @@ search_results = urbanaccess.gtfsfeeds.search(search_text='madison')
 # 4 results will be returned, you can see them by printing the dataframe below
 print('Results from searching for Madison on now-defunct GTFS Data Exchange')
 print(search_results.head(5))
-continue_check()
 
 # add a feed to the gtfs to include in the analysis
 feeds = urbanaccess.gtfsfeeds.feeds
@@ -45,6 +44,8 @@ verbose = True
 bbox = (-89.566399,42.984056,-89.229584,43.171917)
 remove_stops_outsidebbox = True
 append_definitions = True
+# updates these attributes: stops, routes, trips, stop_times, calendar,
+#                           calendar_dates, stop_times_int, headways
 loaded_feeds = urbanaccess.gtfs.load.gtfsfeed_to_df(gtfsfeed_path,
                                                     validation,
                                                     verbose,
@@ -52,24 +53,31 @@ loaded_feeds = urbanaccess.gtfs.load.gtfsfeed_to_df(gtfsfeed_path,
                                                     remove_stops_outsidebbox,
                                                     append_definitions)
 
-print('gtfsfeed_to_df returns processed df of corresponding GTFS feed text files')
-print('Loaded feeds sample of stops:')
-print(loaded_feeds.stops.head())
-print('Loaded feeds sample of routes:')
-print(loaded_feeds.routes.head())
-print('Loaded feeds sample of trips:')
-print(loaded_feeds.trips.head())
-print('Loaded feeds sample of stop_times:')
-print(loaded_feeds.stop_times.head())
-print('Loaded feeds sample of calendar:')
-print(loaded_feeds.calendar.head())
-print('Loaded feeds sample of calendar_dates:')
-print(loaded_feeds.calendar_dates.head())
-print('Loaded feeds sample of stop_times_int:')
-print(loaded_feeds.stop_times_int.head())
-print('Loaded feeds sample of headways:')
-print(loaded_feeds.headways.head())
+# what remains an empty dataframe is stop_times_int, which we still
+# need to generate before we can get to calculating headways
+columns = ['route_id', 'direction_id', 'trip_id', 'service_id', 
+           'unique_agency_id']
+day = 'wednesday' # pick an arbitrary day of week
+tripschedualselector = urbanaccess.gtfs.network.tripschedualselector
+cal_selected_trips = tripschedualselector(
+                                input_trips_df=loaded_feeds.trips[columns],
+                                input_calendar_df=loaded_feeds.calendar,
+                                day=day)
+
+# approximate missing stop times via linear interpolation
+interpolatestoptimes = urbanaccess.gtfs.network.interpolatestoptimes
+intermediate_interpolation = interpolatestoptimes(
+                                stop_times_df=loaded_feeds.stop_times,
+                                calendar_selected_trips_df=cal_selected_trips,
+                                day=day)
+
+# now calculate the difference in top times in new column
+timedifference = urbanaccess.gtfs.network.timedifference
+stop_times_int = timedifference(stop_times_df=intermediate_interpolation)
+
+# now we can update loaded_feeds with this new dataframe
+loaded_feeds.stop_times_int = stop_times_int
 
 # now we need to calculate the headways, given the downloaded gtfs
-headway_timerange =  ['07:00:00','10:00:00'] # approx a.m. peak
+headway_timerange = ['07:00:00','10:00:00'] # approx a.m. peak
 urbanaccess.gtfs.headways.headways(loaded_feeds, headway_timerange)
